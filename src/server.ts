47050db5ac0de31d3a31d90872cf1d7960080861
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-11-12 12:56:43 -0300

    add express.json()

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world' });
-});
+app.use(express.json());
+app.use(routes);
 app.listen(5500, () => console.log('Server Running'));
