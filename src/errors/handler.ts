9edacd94a4c6d30e0cef8b117f307407e82787a1
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-11-12 20:56:30 -0300

    add error treatment to cpf major than 11

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -13,11 +13,15 @@ const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
         });
 
         return res.status(400).json({ message: 'Validation fails', errors });
-    }
-    if (error.code === '23505')
-        return res.json({
+    } else if (error.code === '23505')
+        return res.status(400).json({
             message: 'Insert fails',
             error: error.detail,
+        });
+    else if (error.code === '22001')
+        return res.status(400).json({
+            message: 'Insert fails',
+            error: "cpf must not be major than 11 characters",
         });
     return res.status(500).json({ message: 'Internal Server Error ' });
 };
