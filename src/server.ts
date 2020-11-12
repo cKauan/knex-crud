9b16985ddfef64faf3f474b23e30dbdbd73e1e98
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-11-12 19:56:39 -0300

    add error treatment     - install yup and express-async-errors to treat errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import "express-async-errors";
+import errorHandler from "./errors/handler";
 import routes from './routes';
 
 const app = express();
-
+const port = 5500 || process.env.PORT;
 app.use(express.json());
 app.use(routes);
-app.listen(5500, () => console.log('Server Running'));
+app.use(errorHandler);
+app.listen(port, () => console.log('Server Running'));
