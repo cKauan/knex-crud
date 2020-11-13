d54e9bf558fd1b87d2143abc7be9874b9090f810
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-11-13 09:54:51 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import "express-async-errors";
 import errorHandler from "./errors/handler";
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
-const port = 5500 || process.env.PORT;
+const port = process.env.PORT || 5500;
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
